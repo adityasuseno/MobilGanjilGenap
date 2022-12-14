e4ffc3e575d44a67fc64f83213751988aac3ac70
Aditya Suseno <adit@bitwyre.com>
2022-12-14 21:39:56 +0700

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use chrono::NaiveDate;
+use chrono::{Weekday, Duration, NaiveDate};
 
 fn main() {
-    let year = 2022;
-
+    let tahun: u16 = 2022;
+    let c = NaiveDate::from_ymd_opt(tahun, 0, 1);
+    print!("Start Date {}", c)
+    c + Duration::days(1);
+    println!("Add One day is {}", c)
 }
